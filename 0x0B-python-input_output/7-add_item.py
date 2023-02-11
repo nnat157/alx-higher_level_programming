88a591c7115120ef8080796ebe0500f99861ea80
nnat157 <najibtackie55@gmail.com>
2023-02-11 06:40:44 +0000

    N queens puzzle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """
-9-add_item module
+    Python script that adds all args to a Python List.
+    List is then saved to a file.
 """
 
+
 import sys
+import json
+import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+file = "add_item.json"
+json_list = []
 
-def add_items():
-    """
-    function that adds all arguments to a Python list
-    and then saves them to a file
-    """
-    try:
-        python_list = load_from_json_file('add_item.json')
-    except:
-        python_list = []
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
 
-    for i in range(1, len(sys.argv)):
-        python_list.append(sys.argv[i])
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
 
-    save_to_json_file(python_list, 'add_item.json')
-
-if __name__ == '__main__':
-    add_items()
+save_to_json_file(json_list, file)
