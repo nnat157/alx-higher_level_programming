202d0001ee03b46a4016c6ce1b1a4a0c38ce96b0
nnat157 <najibtackie55@gmail.com>
2023-04-13 08:08:58 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+	console.log('No argument');
+} else if (message === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
+
+
